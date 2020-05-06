6fd95c0a56720dfc78ff9e27fdfd69e994ec8c3a
kabane <kabaneshi@gmail.com>
2020-05-06 10:58:39 +0900

    UsersにTelカラム追加

--- db/migrate/20200505200628_create_users.rb
+++ db/migrate/20200505200628_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
+      t.string :tel
       t.date :birthday
       t.integer :gender
 
