9af8aeea2f906d506b154e30b34822eb2287f86d
kabane <kabaneshi@gmail.com>
2020-05-15 03:07:52 +0900

    不要なファイル削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,14 +20,6 @@ class User < ApplicationRecord
       password: password,
       password_confirmation: password
     )
-    # user = User.new(
-    #   provider: auth.provider,
-    #   uid: auth.uid,
-    #   email: auth.info.email || "#{SecureRandom.hex}@example.com",
-    #   password: password,
-    #   password_confirmation: password
-    # )
-    # user.save!
   end
 
 end
