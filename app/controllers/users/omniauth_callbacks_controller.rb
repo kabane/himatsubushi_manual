c185af68ab60be4a9dac88db856bb544d93d35a6
kabane <kabaneshi@gmail.com>
2020-05-15 03:04:27 +0900

    微修正

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def twitter
     @user = User.find_or_create_for_auth!(request.env["omniauth.auth"])
 
-    puts @user
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: "Twitter") if is_navigational_format?
