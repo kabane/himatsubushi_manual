43503e5df3807362ca29f10f1dfafb77e7210ab6
kabane <kabaneshi@gmail.com>
2020-05-15 02:22:53 +0900

    devise関連の設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'contents#index'
 
   resources :contents, only: [:index, :show]
-  
+
   namespace :admin do
     resources :contents
   end
